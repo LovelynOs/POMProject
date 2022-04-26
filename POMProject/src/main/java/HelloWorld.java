0b10482e252ddaf5a4b78c241745195f20a04cf0
LovelynOs <103640069+LovelynOs@users.noreply.github.com>
2022-04-26 12:10:17 +0100

    For comparison

--- POMProject/src/main/java/HelloWorld.java
+++ POMProject/src/main/java/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
          System.out.println("This is for git rebase from intelliJ ");
-        
+        System.out.println("This is from intelliJ to compare branches");
     }
 }
